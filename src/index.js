81a96c5a37a07719bfcd0390576a6bb0a10776bd
tarr <tarr.mail@yandex.ru>
2020-03-18 09:47:21 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined || array[0] == undefined) {
+    return 0;
+  } else {
+    return Math.min.apply(null, array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined || array[0] == undefined) {
+    return 0;
+  } else {
+    return Math.max.apply(null, array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array[0] == undefined) {
+    return 0;
+  } else {
+    return array.reduce((a, el) => (a + el)) / array.length;
+  }
 }
